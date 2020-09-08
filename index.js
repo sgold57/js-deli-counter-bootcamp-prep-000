5b1fb7db222df23c5aefe6ad079d3481cd3e4a53
carefree-request-1153 <sgold5757@gmail.com>
2020-09-08 19:07:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ function nowServing(deliLine) {
     return `There is nobody waiting to be served!`
   }
   else {
-    return `Currently serving ${deliLine.shift}.`
+    return `Currently serving ${deliLine.shift()}.`
   }
 }
