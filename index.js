da7103db34dd6ae5291159a6f1357d9fea3d3bc8
carefree-request-1153 <sgold5757@gmail.com>
2020-09-08 19:04:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,9 @@ function takeANumber(katzDeli, name) {
   katzDeli.push(name)
   return `Welcome, ${name}. You are number ${katzDeli.length} in line.`
 }
+
+function nowServing(deliLine) {
+  if (deliLine.length === 0) {
+    return `There is nobody waiting to be served!`
+  }
+}
