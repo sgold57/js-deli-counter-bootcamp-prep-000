7958ca017ebceccbad0f4caa2efd936e3e10bd2d
carefree-request-1153 <sgold5757@gmail.com>
2020-09-08 18:58:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 function takeANumber(katzDeli, name) {
   katzDeli.unshift(name)
-  return `Welcome ${name}. You are number ${katzDeli.length} in line.`
+  return `Welcome, ${name}. You are number ${katzDeli.length} in line.`
 }
